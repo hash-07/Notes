1a713f17454367cd2b1b6450c75890a3f84064f3
hasheer <hasheer11@gmail.com>
2020-08-05 13:27:33 +0530

    Updates

---
+++ precache-manifest.2603ecb735bbed32bd90af4e9b514b62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8cd2f9b6e4c8e6c1f275f153cd7e6e",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "8c86d0785d3f4fba7541",
+    "url": "/Notes/static/css/main.88b3630c.chunk.css"
+  },
+  {
+    "revision": "0c29e137634f170a82d2",
+    "url": "/Notes/static/js/2.9224ce33.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Notes/static/js/2.9224ce33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c86d0785d3f4fba7541",
+    "url": "/Notes/static/js/main.0c33c3da.chunk.js"
+  },
+  {
+    "revision": "3a4d8c38e64fac6055f6",
+    "url": "/Notes/static/js/runtime-main.8ee29453.js"
+  },
+  {
+    "revision": "313f200495a8f190da8774ca9704ba0f",
+    "url": "/Notes/static/media/hk.313f2004.png"
+  }
+]);
